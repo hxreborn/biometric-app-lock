ca6d0bc9ab639d7ed9cbfedd54bdb46220416424
official-Arvind <stararvindgaming@gmail.com>
2026-09-19 02:22:17 +0530

    chore: remove remaining scrap file parser.py

--- parser.py
+++
@@ -1,9 +0,0 @@
-﻿import json
-with open('comments_utf8.json', encoding='utf-8-sig') as f:
-    data = json.load(f)
-with open('output.txt', 'w', encoding='utf-8') as out:
-    for c in data[-10:]:
-        out.write(f"File: {c.get('path')} @ line {c.get('line', c.get('original_line'))}\n")
-        out.write(f"User: {c['user']['login']}\n")
-        out.write(f"Body: {c.get('body')}\n")
-        out.write('-'*40 + '\n')
